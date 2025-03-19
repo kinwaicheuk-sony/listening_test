9e3f3248999079054690d7052fa35aa7e6bf0f01
Kin Wai Cheuk <kinwai.cheuk@sony.com>
2025-03-19 12:54:00 +0900

    added code for audio codec conversion

---
+++ wav2x.py
@@ -0,0 +1,31 @@
+# Convert a folder of wav files to flac files
+# Usage: python wav2flac.py <input_folder> <output_folder> <extension>
+# Example: python wav2flac.py /home/username/wav /home/username/flac
+
+import os
+import sys
+import torchaudio
+import tqdm
+
+def wav2x(input_folder, output_folder, extension='flac'):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    for root, _, files in os.walk(input_folder):
+        for file in tqdm.tqdm(files):
+            if file.endswith('.wav'):
+                wav_file = os.path.join(root, file)
+                flac_file = os.path.join(output_folder, file.replace('.wav', f'.{extension}'))
+                waveform, sample_rate = torchaudio.load(wav_file)
+                torchaudio.save(flac_file, waveform, sample_rate=sample_rate)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: python wav2flac.py <input_folder> <output_folder> <extension>')
+        sys.exit(1)
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
+    extension = sys.argv[3] if len(sys.argv) > 3 else 'flac'
+    wav2x(input_folder, output_folder, extension)
+    print('Conversion completed!')
+
+    
